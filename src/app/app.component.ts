0cbea97745015bb536cefd6d4f569263d2330151
ea-elbaz <ea-elbaz@EMAN>
2025-08-19 13:56:55 +0200

    add products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/products';
-import { IPagination } from './models/Pagination';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,21 +11,12 @@ import { IPagination } from './models/Pagination';
 export class AppComponent  implements OnInit{
 
     title = 'Ecommerce';
-    products:IProduct[]=[];
+  
 
-    constructor(private http:HttpClient){}
+    constructor(){}
 
   ngOnInit(): void {
-  this.http.get<IPagination>('https://localhost:7166/api/products/paged?pagesize=50')
-    .subscribe({
-      next: (response) => {
-        this.products = response.data;
-        console.log(response);
-      },
-      error: (err) => {
-        console.error(err);
-      }
-    });
+  
 }
 
 
