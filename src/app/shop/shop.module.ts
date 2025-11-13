d09f3bc03a9418daeb1f3451c94e8fe478eed169
ShimaaAhmedElket <90112999+ShimaaAhmedElket@users.noreply.github.com>
2025-11-13 18:12:06 +0200

    My changes at home page

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemsComponent } from './product-items/product-items.component';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopRoutingModule } from './shop-routing.module';
@@ -14,14 +13,12 @@ import { AppRoutingModule } from '../app-routing.module';
   declarations: [
     ShopComponent,
     ProductItemsComponent,
-    ProductDetailsComponent
-    
+
   ],
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule,
-    ShopRoutingModule
+  
 ],
   exports:[ShopComponent]
 })
