64710ac25c2ec1e1516c8d021844393d194376f5
aya-sakr <ayasakr1000@gmail.com>
2025-12-06 06:57:33 -0800

    finish add to cart

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,29 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { Cart } from '../../shared/models/cart';
+import { CartItem } from 'src/app/shared/models/cartItem';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CartComponent implements OnInit {
   cart: Cart | null = null;
+  CartItems: any = [];
+  subTotal: number = 0;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private toaster: ToastrService
+  ) {}
 
   ngOnInit() {
-    this.cartService.cart$.subscribe(cart => {
-      this.cart = cart;
-    });
+    this.getCartItems();
+  }
+  getCartItems() {
+    if ('cart' in localStorage) {
+      // this.cartService.getItemFromLocalStorage('cart');
+      this.CartItems = JSON.parse(localStorage.getItem('cart')!);
+    }
+
+    this.getSubToalPrice();
+  }
+  getSubToalPrice() {
+    this.subTotal = 0;
+    for (let x in this.CartItems)
+      this.subTotal +=
+        this.CartItems[x].product.price * this.CartItems[x].quantity!;
   }
 
-  removeItem(id: number) {
-    this.cartService.removeItemFromCart(id);
+  decreaseQty(index: number) {
+    if (this.CartItems[index].quantity > 1) {
+      this.CartItems[index].quantity--;
+      localStorage.setItem('cart', JSON.stringify(this.CartItems));
+      this.getSubToalPrice();
+    }
+  }
+  increaseQty(index: number) {
+    this.CartItems[index].quantity++;
+    localStorage.setItem('cart', JSON.stringify(this.CartItems));
+    this.getSubToalPrice();
   }
 
-  clearCart() {
-    this.cartService.clearCart();
+  removeItem(index: number) {
+    this.CartItems.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(this.CartItems));
+    this.getSubToalPrice();
+    this.toaster.info(
+      `${this.CartItems[index].product.name} Removed From Cart`,
+      'Item Removed'
+    );
   }
 }
-
